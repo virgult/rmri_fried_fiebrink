c36d7bdadf98de8eeaccdccfebf7214aaaaa1933
Andrea Martelloni <a.martelloni@qmul.ac.uk>
2019-11-26 15:01:33 +0000

    Directory structure done right now (hopefully)

--- stl10_autoencoder.py
+++ stl10_autoencoder.py
@@ -2,13 +2,15 @@ import numpy as np
 import compress_pickle
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__)))
 
-from StackedAutoencoder import *
-from STL10Loader import *
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 
-def autoencoder_train(dataset="stl10_dataset.pickle.gz"):
+from StackedAutoencoder.StackedAutoencoder import StackedAutoencoderTrain, DeepAutoencoderTrain
+from STL10.STL10Loader import STL10Loader
+
+
+def stacked_autoencoder_train(dataset="stl10_dataset.pickle.gz"):
     stl10_dataset = compress_pickle.load(dataset)
     ((x_train, y_train), (x_test, y_test)) = stl10_dataset.data(flattened=True)
     # Create and train stacked autoencoders
@@ -22,5 +24,22 @@ def autoencoder_train(dataset="stl10_dataset.pickle.gz"):
     print("Done.")
     return s
 
+
+def deep_autoencoder_train(dataset="stl10_dataset.pickle.gz"):
+    stl10_dataset = compress_pickle.load(dataset)
+    ((x_train, y_train), (x_test, y_test)) = stl10_dataset.data(flattened=True)
+    # Create and train stacked autoencoders
+    s = DeepAutoencoderTrain()
+    print("Training autoencoder...")
+    s.train_autoencoder([1024], x_train, y_train, x_test, y_test)
+    #print("Training second layer...")
+    #s.train_second_layer(32)
+    #print("Training classifier...")
+    #s.train_classifier(stl10_dataset.class_names)
+    print("Done.")
+    s.plot_model_performance()
+    return s
+
+
 if __name__ == "__main__":
-    autoencoder_train()
+    deep_autoencoder_train()
