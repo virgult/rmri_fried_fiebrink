885dc0602f5ddc2ac4fb0c85a91f603753ff3099
Andrea Martelloni <am360@kapoor.eecs.qmul.ac.uk>
2019-11-28 16:14:05 +0000

    This looks exciting for STL-10 training...

--- stl10_autoencoder.py
+++ stl10_autoencoder.py
@@ -33,9 +33,9 @@ def deep_autoencoder_train(dataset="stl10_dataset.pickle.gz"):
     # Create and train stacked autoencoders
     s = DeepAutoencoderTrain()
     print("Training autoencoder...")
-    s.train_autoencoder([64, 16], x_train, y_train, x_test, y_test, n_epochs=25)
+    s.train_autoencoder([256, 128, 64], x_train, y_train, x_test, y_test, n_epochs=50)
     print("Training classifier...")
-    s.train_classifier(stl10_dataset.class_names, n_epochs=25)
+    s.train_classifier(stl10_dataset.class_names, n_epochs=50)
     print("Done.")
     s.plot_model_performance()
     return s
